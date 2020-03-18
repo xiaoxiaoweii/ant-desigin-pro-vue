47d3c71d30322b9d1ae8b2a5289d48600d241033
xiaoxiaoweii <2679656468@qq.com>
2020-03-18 23:58:30 +0800

    使用第三方图表Echarts

---
+++ src/components/Chart.vue
@@ -0,0 +1,55 @@
+<template>
+  <div ref="chartDom" style="height: 400px"></div>
+</template>
+<script>
+import echarts from "echarts";
+import debounce from "lodash/debounce";
+import { addListener, removeListener } from "resize-detector";
+export default {
+  props: {
+    option: {
+      type: Object,
+      default: () => {}
+    }
+  },
+  created() {
+    // resize防抖处理 防止一定时间内resize执行多次
+    this.resize = debounce(this.resize, 300);
+  },
+  watch: {
+    // 监听options
+    option(val) {
+      this.chart.setOption(val);
+    }
+    // 深度监听options
+    // options: {
+    //   handle(val) {
+    //     this.chart.setOption(val);
+    //   },
+    //   deep: true
+    // }
+  },
+  mounted() {
+    this.renderChart();
+    // 添加监听事件
+    addListener(this.$refs.chartDom, this.resize);
+  },
+  beforeDestroy() {
+    // 移除监听事件
+    removeListener(this.$refs.chartDom, this.resize);
+    this.chart.dispose();
+    this.chart = null;
+  },
+  methods: {
+    resize() {
+      console.log("resize");
+      this.chart.resize();
+    },
+    renderChart() {
+      // 基于准备好的dom，初始化echarts实例
+      this.chart = echarts.init(this.$refs.chartDom);
+      this.chart.setOption;
+    }
+  }
+};
+</script>
--- src/views/Dashboard/Analysis.vue
+++ src/views/Dashboard/Analysis.vue
@@ -1,9 +1,51 @@
 <template>
   <div>
     Analysis 分析页
+    <Chart :option="chartOption" style="height: 400px" />
   </div>
 </template>
 <script>
-export default {};
+import Chart from "../../components/Chart";
+import random from "lodash/random";
+export default {
+  data() {
+    return {
+      // 图标信息
+      chartOption: {
+        title: {
+          text: "ECharts 入门示例"
+        },
+        tooltip: {},
+        xAxis: {
+          data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+        },
+        yAxis: {},
+        series: [
+          {
+            name: "销量",
+            type: "bar",
+            data: [5, 20, 36, 10, 10, 20]
+          }
+        ]
+      }
+    };
+  },
+  mounted() {
+    this.interval = setInterval(() => {
+      // echarts chartOption 赋予为100以内的随机数
+      this.chartOption.series[0].data = this.chartOption.series[0].data.map(
+        () => random(100)
+      );
+      this.chartOption = { ...this.chartOption };
+    }, 1500);
+  },
+  beforeDestroy() {
+    // 销毁interval
+    clearInterval(this.interval);
+  },
+  components: {
+    Chart
+  }
+};
 </script>
 <style scoped></style>
