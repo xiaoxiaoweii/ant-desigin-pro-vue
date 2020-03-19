20fa15942fbd3e6d8ba7d06c17b348b015133e0e
xiaoxiaoweii <2679656468@qq.com>
2020-03-20 00:09:18 +0800

    初始数据,自动校验,自动赋值

--- src/views/Forms/BasicForm.vue
+++ src/views/Forms/BasicForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <a-form :layout="formLayout">
+  <a-form :layout="formLayout" :form="form">
     <a-form-item
       label="Form Layout"
       :label-col="formItemLayout.labelCol"
@@ -9,37 +9,36 @@
         default-value="horizontal"
         @change="handleFormLayoutChange"
       >
-        <a-radio-button value="horizontal">
-          Horizontal
-        </a-radio-button>
-        <a-radio-button value="vertical">
-          Vertical
-        </a-radio-button>
-        <a-radio-button value="inline">
-          Inline
-        </a-radio-button>
+        <a-radio-button value="horizontal">Horizontal</a-radio-button>
+        <a-radio-button value="vertical">Vertical</a-radio-button>
+        <a-radio-button value="inline">Inline</a-radio-button>
       </a-radio-group>
     </a-form-item>
     <a-form-item
       label="Field A"
       :label-col="formItemLayout.labelCol"
       :wrapper-col="formItemLayout.wrapperCol"
-      :validateStatus="fieldAStatus"
-      :help="fieldAHelp"
     >
-      <a-input v-model="fieldA" placeholder="input placeholder" />
+      <a-input
+        v-decorator="[
+          'fieldA',
+          {
+            initialValue: fieldA,
+            rules: [{ required: true, min: 6, message: '必须大于5个字符' }]
+          }
+        ]"
+        placeholder="input placeholder"
+      />
     </a-form-item>
     <a-form-item
       label="Field B"
       :label-col="formItemLayout.labelCol"
       :wrapper-col="formItemLayout.wrapperCol"
     >
-      <a-input v-model="fieldB" placeholder="input placeholder" />
+      <a-input v-decorator="['fieldB']" placeholder="input placeholder" />
     </a-form-item>
     <a-form-item :wrapper-col="buttonItemLayout.wrapperCol">
-      <a-button type="primary" @click="handleSubmit">
-        Submit
-      </a-button>
+      <a-button type="primary" @click="handleSubmit">Submit</a-button>
     </a-form-item>
   </a-form>
 </template>
@@ -47,25 +46,22 @@
 <script>
 export default {
   data() {
+    // 创建form实例
+    this.form = this.$form.createForm(this);
     return {
       formLayout: "horizontal",
-      fieldA: "",
-      fieldB: "",
-      fieldAStatus: "",
-      fieldAHelp: ""
+      fieldA: "hello",
+      fieldB: ""
     };
   },
-  watch: {
-    // 监听fieldA的变化
-    fieldA(val) {
-      if (val.length <= 5) {
-        this.fieldAStatus = "error";
-        this.fieldAHelp = "必须大于5个字符";
-      } else {
-        this.fieldAStatus = "";
-        this.fieldAHelp = "";
-      }
-    }
+  mounted() {
+    setTimeout(() => {
+      // 改变表单的值
+      console.log("改变表单的数值");
+      this.form.setFieldsValue({
+        fieldA: "hello world"
+      });
+    }, 3000);
   },
   computed: {
     formItemLayout() {
@@ -91,17 +87,18 @@ export default {
       this.formLayout = e.target.value;
     },
     handleSubmit() {
-      if (this.fieldA.length <= 5) {
-        this.fieldAStatus = "error";
-        this.fieldAHelp = "必须大于5个字符";
-      } else {
-        // this.fieldAStatus = ''
-        // this.fieldAHelp = ''
-        console.log({
-          fieldA: this.fieldA,
-          fieldB: this.fieldB
-        });
-      }
+      this.form.validateFields((err, values) => {
+        // 如果没有错误信息
+        if (!err) {
+          // 打印表单
+          console.log(values);
+          this.fieldA = values.fieldA;
+          this.fieldB = values.fieldB;
+          // 当表单数据比较多时
+          // 将values的值都同步到this上
+          Object.assign(this, values);
+        }
+      });
     }
   }
 };
